50aa65df8eb18551e6826bece684bd4530289462
Bohár Dávid <89093467+F4llingf0x@users.noreply.github.com>
2023-10-08 22:16:10 +0200

    Chore: data.sql added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Project (Name, Status, Unit, Creation_Date) VALUES ('Project 1', 'ACTIVE', 'MM', '2023-01-15');
+INSERT INTO Project (Name, Status, Unit, Creation_Date, Update_Date) VALUES ('Project 2', 'ACTIVE', 'M', '2023-01-15', '2023-10-08');
+INSERT INTO Project (Name, Status, Unit, Creation_Date) VALUES ('Project 3', 'ACTIVE', 'MM', '2023-10-08');
